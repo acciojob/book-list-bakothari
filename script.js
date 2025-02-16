4c16e2d59763966b4dda1816661fabc37066b823
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-16 16:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ document.getElementById("submit").addEventListener("click", (event)=>{
+       
+        let title = document.getElementById("title").value
+        let author = document.getElementById("author").value
+        let isbn = document.getElementById("isbn").value
+        let table = document.querySelector(".book-list");
+        let row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${title}</td>
+            <td>${author}</td>
+            <td>${isbn}</td>
+            <td><button class="delete">Delete</button></td>
+        `;
+        if (title === "" || author === "" || isbn === "") {
+            alert("Please fill in all fields.");
+            return;
+        }
+        table.appendChild(row);
+        document.getElementById("title").value = "";
+        document.getElementById("author").value = "";
+        document.getElementById("isbn").value = "";
+    });
+    document.querySelector(".book-list").addEventListener("click", function (event) {
+        if (event.target.tagName === "BUTTON") {
+            event.target.parentElement.parentElement.remove();
+        }
+    });
+
